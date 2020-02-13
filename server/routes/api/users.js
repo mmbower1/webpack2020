159f816d1c602cd11d58c703214790e9c07d6907
Matt Bower <MB2@joness-imac.lan>
2020-02-13 15:26:03 -0800

    add mongoose, api/route/user

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,8 +1,33 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
 const router = express.Router();
+const User = require('../../models/User');
 
-router.get('/', (req, res) => {
-    res.json('Users.js')
+router.get('/', async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const { email } = req.body;
+    try {
+        let user = await User.findOne({ email }, { unqiue: true });
+        if (user) {
+            return res.status(400).json({ errors: [{ msg: 'Email already taken!' }] });
+        }
+        // create new instance, needs to be before bcrypt
+        user = new User({
+            email,
+        });
+        // save instance to mongo
+        await user.save()
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error users');
+    }
+
+    const user = res.json('Users.js')
+    await user.save()
 });
 
 module.exports = router
--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const path = require('path');
 const port = process.env.PORT || 5000;
 
 // const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+connectDB();
 
 if (process.env.NODE_ENV != 'production') {
     require('dotenv').config()
@@ -21,8 +22,6 @@ if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
     })
 }
-
-connectDB();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
